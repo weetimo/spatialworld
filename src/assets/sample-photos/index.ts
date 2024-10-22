ee0271c6c43fbb1c85ea9b7ce9529daba0c53c55
caitlin.ai/ <chiangcaitlin2003@gmail.com>
2024-10-22 16:03:08 +0800

    add: sample images

---
+++ src/assets/sample-photos/index.ts
@@ -0,0 +1,4 @@
+export { default as garden0 } from './garden0.jpg'
+export { default as garden1 } from './garden1.jpg'
+export { default as garden2 } from './garden2.jpg'
+export { default as garden3 } from './garden3.jpg'
