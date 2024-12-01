ccbb8362bc4f11fc903dd506fd9f8b25f1a2198e
Timothy W <timothywee2@gmail.com>
2024-12-01 22:29:40 +0800

    debugging display issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/spatialworld',
+  base: '/spatialworld/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
   },
 })
