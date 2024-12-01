32818f59a122effd722e90da49b7a935096634d2
Timothy W <timothywee2@gmail.com>
2024-12-01 21:23:09 +0800

    updated display configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/spatialworld/' : '/',
+  base: '/spatialworld/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+  },
 })
