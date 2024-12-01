8b45135a2050f3a492941e3fa92d85630d22077b
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2024-12-01 13:57:28 +0800

    New firebase project

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,10 +4,11 @@ import { getDatabase } from 'firebase/database'
 const firebaseConfig = {
   apiKey: "AIzaSyBF6BDefsC6zmhnuRmk04l_AHCPbp5fqn8",
   authDomain: "spatial-world.firebaseapp.com",
+  databaseURL: "https://spatial-world-default-rtdb.firebaseio.com",
   projectId: "spatial-world",
-  storageBucket: "spatial-world.appspot.com",
+  storageBucket: "spatial-world.firebasestorage.app",
   messagingSenderId: "558541049241",
-  appId: "1:558541049241:web:5be52ac880bf83644b9708"
+  appId: "1:558541049241:web:b7a35d3b634472814b9708"
 }
 
 const app = initializeApp(firebaseConfig)
