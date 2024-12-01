d227c95827d6854340f9750c91f28b692060cb26
Timothy W <timothywee2@gmail.com>
2024-12-01 21:07:39 +0800

    Add default route redirect with demo ID

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { FeedDetail, ImageFeed, ImageWorkshop, MeetTheCharacters, UserParticulars, UserPreferences, Welcome, Admin, Admin_home } from './features'
 
 function App() {
+  const defaultId = 'demo' // You can change this to any default ID you want
+
   return (
     <Router>
       <Routes>
-        <Route path="/:id" element={<Welcome />} />
+        <Route path="/" element={<Navigate to={`/welcome/${defaultId}`} />} />
+        <Route path="/welcome" element={<Navigate to={`/welcome/${defaultId}`} />} />
+        <Route path="/welcome/:id" element={<Welcome />} />
         <Route path="/user-details/:id" element={<UserParticulars />} />
         <Route path="/user-preferences/:id" element={<UserPreferences />} />
         <Route path="/meet-characters/:id" element={<MeetTheCharacters />} />
