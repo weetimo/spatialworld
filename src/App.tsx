e6d0605707750106ccc17262c38b4c8a63afc399
Timothy W <timothywee2@gmail.com>
2024-12-01 20:15:05 +0800

    deploy config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { FeedDetail, ImageFeed, ImageWorkshop, MeetTheCharacters, UserParticulars, UserPreferences, Welcome, Admin, Admin_home } from './features'
 
