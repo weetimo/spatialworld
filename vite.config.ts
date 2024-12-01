968711359de9c0a598c3748cb937a570ff3319f5
Timothy W <timothywee2@gmail.com>
2024-12-01 21:31:44 +0800

    resolving blank screen issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/spatialworld/',
+  base: '/spatialworld',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
