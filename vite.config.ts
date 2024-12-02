b88f27e0811cecf204a5af6cc75d1e09015abed8
Timothy W <timothywee2@gmail.com>
2024-12-02 10:49:30 +0800

    resolving pathing & building in browser

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/spatialworld/",
+  base: "./",
   build: {
     outDir: 'dist',
     sourcemap: true,
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
       }
     }
   }
