d38b31a8f0cd25a1e55895ce479c9202200b37ef
Timothy W <timothywee2@gmail.com>
2024-12-02 11:49:00 +0800

    modified vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
-  base: '/spatialworld/',
   build: {
     outDir: 'dist',
     sourcemap: true,
