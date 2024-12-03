f655bf9005ba7325f12b2713447ebd2b5cb03502
Timothy W <timothywee2@gmail.com>
2024-12-03 13:56:04 +0800

    updated dockerfile and requirements.txt to work

--- backend/topic_modelling.py
+++ backend/topic_modelling.py
@@ -6,8 +6,9 @@ from typing import List
 import os
 
 load_dotenv()
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key=OPENAI_API_KEY)
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),  # This is the default and can be omitted
+)
 
 def GenerateTopics(prompts: List[str]):
     combined_text = "\n\n".join(prompts)  
