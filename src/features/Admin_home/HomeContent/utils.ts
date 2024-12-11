1703f63d6a57037652aa08865e27330acac46a8e
cweim <119029449+cweim@users.noreply.github.com>
2024-12-11 15:03:50 +0800

    update stopword - wordcloud

--- src/features/Admin_home/HomeContent/utils.ts
+++ src/features/Admin_home/HomeContent/utils.ts
@@ -134,12 +134,12 @@ export const word_cloud = (generations: Generation[]): WordCloudData[] => {
     'few', 'for', 'from', 'further', 'generate','had', 'hadn’t', 'has', 'hasn’t', 'have', 'haven’t', 'having', 'he', 'he’d',
     'he’ll', 'he’s', 'her', 'here', 'here’s', 'hers', 'herself', 'him', 'himself', 'his', 'how', 'how’s', 'i',
     'i’d', 'i’ll', 'i’m', 'i’ve', 'if', 'in', 'into', 'is', 'isn’t', 'it', 'it’s', 'its', 'itself', 'let’s', 'me',
-    'more', 'most', 'mustn’t', 'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or',
+    'more', 'most','make', 'mustn’t', 'my', 'myself', 'no', 'nor', 'not', 'of', 'off', 'on', 'once', 'only', 'or',
     'other', 'ought', 'our', 'ours', 'ourselves', 'out', 'over', 'own','users','user', 'same', 'shan’t', 'she', 'she’d',
     'she’ll', 'she’s', 'should', 'shouldn’t', 'so', 'some','see', 'such', 'than', 'that', 'that’s', 'the', 'their',
     'theirs', 'them', 'themselves', 'then', 'there', 'there’s', 'these', 'they', 'they’d', 'they’ll', 'they’re',
     'they’ve', 'this', 'those', 'through', 'to', 'too', 'under', 'until', 'up', 'very', 'was', 'wasn’t', 'we',
-    'we’d', 'we’ll', 'we’re', 'we’ve', 'were', 'weren’t', 'what', 'what’s', 'when', 'when’s', 'where', 'where’s',
+    'we’d', 'we’ll','well', 'we’re', 'we’ve', 'were', 'weren’t', 'what','want', 'what’s', 'when', 'when’s', 'where', 'where’s',
     'which', 'while', 'who', 'who’s', 'whom', 'why', 'why’s', 'with', 'won’t', 'would', 'wouldn’t', 'you', 'you’d',
     'you’ll', 'you’re', 'you’ve', 'your', 'yours', 'yourself', 'yourselves','1','2','3','4','5','6','7','8','9','10'
 ]);
